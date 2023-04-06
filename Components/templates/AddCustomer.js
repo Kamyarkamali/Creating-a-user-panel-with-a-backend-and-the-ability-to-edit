6f25687e69247b7cc79bc90fb3bf630536fa77d6
kamyar kamali kamazani <kamyarkamali021@gmail.com>
2023-04-07 04:22:32 +0430

    Building a custom function for saving, cancelling, and completing it in the FrontEnd.

--- Components/templates/AddCustomer.js
+++ Components/templates/AddCustomer.js
@@ -1,8 +1,10 @@
 import React,{useState} from 'react'
+import { useRouter } from 'next/router'
 ///Component Module
 import Form from '../module/Form'
 
 function AddCustomer() {
+    const router=useRouter()
     const [form,setForm]=useState({
         name:"",
         Lastname:"",
@@ -11,13 +13,40 @@ function AddCustomer() {
         date:"",
         products:[]
     })
+
+    const cancelHandeler=()=>{
+        setForm({
+            name:"",
+            Lastname:"",
+            phone:"",
+            address:"",
+            date:"",
+            products:[]
+        })
+        router.push("/")
+    }
+
+    const savelHandeler=async()=>{
+        const res= await fetch("/api/customer",{
+            method:"POST",
+            body:JSON.stringify({data:form}),
+            headers:{"Content-Type":"application/json"}
+        })
+        const data=await res.json()
+        console.log(data)
+        
+        if(data.status==="Succsess") router.push("/")
+    }
+
+
+
   return (
     <div className='customer-page'>
         <h4>Add New Item</h4>
         <Form form={form} setForm={setForm}/>
         <div className='customer-page__buttons'>
-            <button className='first'>Cancel</button>
-            <button className='second'>Save</button>
+            <button className='first' onClick={cancelHandeler}>Cancel</button>
+            <button className='second' onClick={savelHandeler}>Save</button>
         </div>
     </div>
   )
